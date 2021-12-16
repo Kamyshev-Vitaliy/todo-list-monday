b716b81346645df907a09c3267dca085001b11a5
User <vitalik.kamyshev.1997@mail.ru>
2021-12-16 23:37:35 +0300

    Creating a function for filtering buttons.Typing the filter state

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,63 @@
 import React, {useState} from 'react';
 import './App.css';
-import {TodoList} from "./components/TodoList";
+import {Todolist} from './Todolist';
 
+export type FilterTaskType = 'All' | 'Active' | 'Completed';
 
 export const App = () => {
-  const taskKey1 = 'taskID1';
-  const taskKey2 = 'taskID2';
 
-  const [task, setTask] = useState({
-    [taskKey1]: [
-      {id: 1, title: 'React', isDone: true},
-      {id: 2, title: 'TypeScript', isDone: false},
-      {id: 3, title: 'Redux', isDone: false},
-      {id: 4, title: 'CSS', isDone: true},
-    ],
-    [taskKey2]: [
-      {id: 5, title: 'Milk', isDone: true},
-      {id: 6, title: 'Fresh', isDone: false},
-      {id: 7, title: 'Bread', isDone: false},
-      {id: 8, title: 'Cheaps', isDone: true},
-    ]
-  })
+  const [task, setTask] = useState([
+    {id: 1, title: "HTML&CSS", isDone: true},
+    {id: 2, title: "JS", isDone: true},
+    {id: 3, title: "ReactJS", isDone: false},
+    {id: 4, title: "Redux", isDone: false}
+  ])
 
+  const [filter, setFilter] = useState<FilterTaskType>('All');
 
-  const deleteTasksItem = (id: number) => {
-    console.log([taskKey1])
+  let newReturnStatusTask;//Создаем копию для обработки тасок с поступающим типом
+
+  switch (filter) {
+    case 'Active':
+      newReturnStatusTask = task.filter((f) => !f.isDone);
+      break;
+    case 'Completed':
+      newReturnStatusTask = task.filter((f) => f.isDone);
+      break;
+    default:
+      newReturnStatusTask = task;
   }
+
+  // let tasks1 = [
+  //   {id: 1, title: "HTML&CSS", isDone: true},
+  //   {id: 2, title: "JS", isDone: true},
+  //   {id: 3, title: "ReactJS", isDone: false}
+  // ]
+  // const tasks2 = [
+  //   {id: 1, title: "Fresh", isDone: true},
+  //   {id: 2, title: "Ice cream", isDone: false},
+  //   {id: 3, title: "Milk", isDone: true}
+  // ]
+
+
+  const removeTaskItemCallback = (mId: number) => {
+    setTask(task.filter((f) => f.id !== mId))
+  }
+
+  const filterTaskTypeCallBack = (statusTask: FilterTaskType) => {
+    setFilter(statusTask)
+  }
+
 
   return (
     <div className="App">
-      {/*<TodoList title={[taskKey1]}*/}
-      {/*          task={task[taskKey1]}*/}
-      {/*          deleteTasksItem={deleteTasksItem}*/}
-      {/*          key={taskKey1}/>*/}
-
-      <TodoList title={'What to by'}
-                task={task[taskKey2]}
-                deleteTasksItem={deleteTasksItem}
-                key={taskKey2}/>
+      <Todolist title={'What to learn'}
+                tasks={newReturnStatusTask}
+                removeTaskItemCallback={removeTaskItemCallback}
+                filterTaskTypeCallBack={filterTaskTypeCallBack}
+      />
+      {/*<Todolist title="What to by" tasks={tasks2}*/}
+      {/*          removeTaskItem={removeTaskItem}/>*/}
     </div>
   );
 }
